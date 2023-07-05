8ed4dbb7db0a6f3fed1574b816bec10fe9d94529
Miguel Tamayo <miguelt2296@gmail.com>
2023-07-05 16:26:56 -0500

    first commit of DOM manipulations and projects

---
+++ toppings-de-pizza/app.js
@@ -0,0 +1,10 @@
+console.log("\nContenedor");
+const contenedor = document.getElementById('contenedor')
+console.log(contenedor);
+console.log(contenedor.innerHTML);
+console.log(typeof  contenedor.innerHTML);
+
+console.log("\n\nTitulo");
+const titulo = document.getElementById('titulo');
+console.log(titulo);
+
